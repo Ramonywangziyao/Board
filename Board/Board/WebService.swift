d0f9119b1452a67d00a155010168ed0e95fa3101
Yu Wang <wangyu2165@gmail.com>
2015-12-26 00:38:46 -0800

    Draft base bill and balance view and workspace structures

---
+++ Board/Board/WebService.swift
@@ -0,0 +1,13 @@
+//
+//  WebService.swift
+//  Board
+//
+//  Created by Wang Yu on 12/26/15.
+//  Copyright © 2015 Wang Yu. All rights reserved.
+//
+
+import UIKit
+
+class WebService: NSObject {
+
+}
