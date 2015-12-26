223682db7a3da1b580ee86ab7e8543fa3889a5af
Yu Wang <wangyu2165@gmail.com>
2015-12-26 15:22:53 -0800

    test empty data set succeed

--- Board/Board/BillTableViewExtension.swift
+++ Board/Board/BillTableViewExtension.swift
@@ -11,7 +11,7 @@ import UIKit
 extension BillViewController: UITableViewDelegate, UITableViewDataSource {
 
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 0
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
