f08c0e47ca93a11ad85f384e773128b69e3afc34
Yu Wang <wangyu2165@gmail.com>
2015-12-26 15:21:50 -0800

    bill controller empty data set and table view extensions

---
+++ Board/Board/BillEmptyDataExtension.swift
@@ -0,0 +1,37 @@
+//
+//  DZNEmptyDataExtension.swift
+//  Board
+//
+//  Created by Wang Yu on 12/26/15.
+//  Copyright © 2015 Wang Yu. All rights reserved.
+//
+
+import UIKit
+import DZNEmptyDataSet
+
+extension BillViewController: DZNEmptyDataSetSource, DZNEmptyDataSetDelegate {
+
+    func imageForEmptyDataSet(scrollView: UIScrollView!) -> UIImage! {
+        return UIImage(named: "empty_box_img")
+    }
+    
+    func titleForEmptyDataSet(scrollView: UIScrollView!) -> NSAttributedString! {
+        let textAttributes = [
+            NSForegroundColorAttributeName: UIColor.lightGrayColor(),
+            NSFontAttributeName: UIFont(name: "Helvetica", size: 17)!,
+        ]
+        return NSAttributedString(string: "No Transection", attributes: textAttributes)
+    }
+    
+    func descriptionForEmptyDataSet(scrollView: UIScrollView!) -> NSAttributedString! {
+        let textAttributes = [
+            NSForegroundColorAttributeName: UIColor.lightGrayColor(),
+            NSFontAttributeName: UIFont(name: "Helvetica", size: 14)!,
+        ]
+        return NSAttributedString(string: "There is no record around, please press the add button below to start your first bill.", attributes: textAttributes)
+    }
+    
+    func emptyDataSetShouldAllowScroll(scrollView: UIScrollView!) -> Bool {
+        return true
+    }
+}
---
+++ Board/Board/BillTableViewExtension.swift
@@ -0,0 +1,26 @@
+//
+//  BillTableViewExtension.swift
+//  Board
+//
+//  Created by Wang Yu on 12/26/15.
+//  Copyright © 2015 Wang Yu. All rights reserved.
+//
+
+import UIKit
+
+extension BillViewController: UITableViewDelegate, UITableViewDataSource {
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+    
+}
