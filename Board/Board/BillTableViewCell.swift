227136487d4458ad640233fb8352092953b5c76e
Yu Wang <wangyu2165@gmail.com>
2015-12-26 17:21:53 -0800

    bill view controller now work with custom cell

---
+++ Board/Board/BillTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  BillTableViewCell.swift
+//  Board
+//
+//  Created by Wang Yu on 12/26/15.
+//  Copyright © 2015 Wang Yu. All rights reserved.
+//
+
+import UIKit
+
+class BillTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var backView: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
