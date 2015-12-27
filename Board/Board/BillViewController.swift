f984fdfe3c07213a8afc3a6c0df6b0465049e043
Yu Wang <wangyu2165@gmail.com>
2015-12-26 17:25:39 -0800

    Change status bar default color to light content

--- Board/Board/BillViewController.swift
+++ Board/Board/BillViewController.swift
@@ -15,7 +15,6 @@ class BillViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
 
--- Board/Board/LeftMenuViewController.swift
+++ Board/Board/LeftMenuViewController.swift
@@ -16,10 +16,5 @@ class LeftMenuViewController: UIViewController, RESideMenuDelegate {
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
 }
