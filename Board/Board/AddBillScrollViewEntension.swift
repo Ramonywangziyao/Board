ceb132e2e2a58f65827dce0e3d6efbe1a0165b73
Yu Wang <wangyu2165@gmail.com>
2015-12-31 16:41:26 -0800

    configure add bill view

---
+++ Board/Board/AddBillScrollViewEntension.swift
@@ -0,0 +1,21 @@
+//
+//  AddBillScrollViewEntension.swift
+//  Board
+//
+//  Created by Wang Yu on 12/31/15.
+//  Copyright © 2015 Wang Yu. All rights reserved.
+//
+
+import UIKit
+
+class AddBillScrollViewEntension: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
